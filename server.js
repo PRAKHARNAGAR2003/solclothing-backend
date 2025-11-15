679b13a6175abb588cb3b2a01086da9d30b2910d
PRAKHAR NAGAR <109655744+PRAKHARNAGAR2003@users.noreply.github.com>
2025-11-15 18:48:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: FRONTEND,
+    origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
