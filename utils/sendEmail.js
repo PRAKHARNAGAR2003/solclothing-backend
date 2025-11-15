774d785d1b532943acd6e2c7b6a49c4e96014c2b
PRAKHAR NAGAR <109655744+PRAKHARNAGAR2003@users.noreply.github.com>
2025-11-15 22:13:09 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,16 +4,16 @@ const sendEmail = async ({ email, subject, message }) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // TLS
+      port: 465,
+      secure: true, // Gmail requires SSL
       auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        pass: process.env.SMTP_PASS, // MUST be Gmail App Password
       },
     });
 
     const mailOptions = {
-      from: `"SolClothing" <${process.env.SMTP_USER}>`,
+      from: "SolClothing" <${process.env.SMTP_USER}>,
       to: email,
       subject,
       html: message,
